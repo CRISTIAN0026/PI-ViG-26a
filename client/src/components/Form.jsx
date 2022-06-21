a4f3b521f96c03a247e51d11ed9e84b08afac935
CRISTIAN0026 <92878127+CRISTIAN0026@users.noreply.github.com>
2022-06-21 11:36:44 -0500

    firts commit

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,14 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getGenres, addGame, getPlatform } from '../redux/actions';
-//import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
+const validate = () => {
+    let errors = {}
+    if (input.name.length < 2 ) errors.name = 'Add recipe name';
+    for (let i = 0; i < input.name.length; i++) {
+        let num = "0123456789-.,;:_/*-+?'¡¿(){}[]><$!#&%=`´¨çÇ"
+        for (let j = 0; j < num.length; j++) {
+            if(input.name[i] === num[j]){
+                errors.name = "only letters"
+            }
+        }
+    }
+    if (input.description.length < 2) errors.description = 'Add a description';
+    for (let k = 0; k < input.description.length; k++) {
+        let numm = "0123456789-.,;:_/*-+?'¡¿(){}[]><$!#&%=`´¨çÇ"
+        for (let l = 0; l < numm.length; l++) {
+            if(input.description[k] === numm[l]){
+                errors.description = "only letters"
+            }
+        }
+    }
+    if (input.rating < 1 || input.rating > 5) errors.rating = 'add a rating of 1 out of 5';
+    if(input.genres.length > 5) {
+        errors.genres = "Genres limit reached"
+    }
+    if(input.platforms.length > 5) {
+        errors.genres = "Platforms limit reached"
+    }
+    return errors;
+}
 
 const Form = () =>{
-    let dispatch = useDispatch()
-    let gen = useSelector(state => state.genres)
-    let plat= useSelector(state => state.platform) 
-    //let nav = useNavigate()
+    let dispatch = useDispatch();
+    let gen = useSelector(state => state.genres);
+    let plat= useSelector(state => state.platform);
+    const [errors, setErrors] = useState({}); 
+    let nav = useNavigate();
 
     const [input, setInput] = useState({
         name:'',
@@ -18,25 +48,27 @@ const Form = () =>{
         rating:'',
         platforms:[],
         genres:[]
-
-    })
+    });
 
     useEffect(() =>{
         dispatch(getGenres())
         dispatch(getPlatform())
-    }, [dispatch])
+    }, [dispatch]);
 
     const handleChange = (e) =>{
         setInput({
             ...input,
             [e.target.name] : e.target.value
         })
-    }
+    };
     
     const handleOnSub = (e) =>{
         e.preventDefault()
+        if(errors.name || errors.description || errors.rating 
+        || errors.diets || (input.name === '')) return alert("add date")
         dispatch(addGame(input))
-    }
+        nav("/home", { replace : true });
+    };
 
     const handleSelect = (e) =>{
         setInput({
@@ -44,19 +76,25 @@ const Form = () =>{
             genres: [...input.genres, e.target.value]
         })
         console.log(input.genres)
-    }
+    };
     
     const handleSelect2 = (e) => {
         setInput(input => ({
             ...input,
             platforms: [...input.platforms, e.target.value]
         }))
-    }
+    };
+
+    setErrors(validate({
+        ...input,
+        [e.target.name]: e.target.value
+    }));
 
 
     return(
         <div>
             <form onSubmit={(e) => handleOnSub(e)}>
+            <label>Genre</label>
             <select onChange={(e) => handleSelect(e)}>
             {
                 gen?.map(g => (
@@ -64,6 +102,12 @@ const Form = () =>{
                 ))
             }
             </select>
+            {
+                errors.genres && (
+                    <p>{errors.genres}</p>
+                )
+            }
+            <label>Platforms</label>
             <select onChange={e => handleSelect2(e)}>
             {
                 plat?.map(p => (
@@ -71,14 +115,34 @@ const Form = () =>{
                 ))
             }
             </select>
+            {
+                errors.platforms && (
+                    <p>{errors.platforms}</p>
+                )
+            }
             <label>Name</label>
             <input type="text" name='name' value={input.name} onChange={(e) => handleChange(e)}/>
+            {
+                errors.name && (
+                    <p>{errors.name}</p>
+                )
+            }
             <label>Description</label>
             <input type="text" name='description' value={input.description} onChange={(e) => handleChange(e)}/>
+            {
+                errors.description && (
+                    <p>{errors.description}</p>
+                )
+            }
             <label>Released</label>
-            <input type="text" name='released' value={input.released} onChange={(e) => handleChange(e)}/>
+            <input type="date" name='released' value={input.released} onChange={(e) => handleChange(e)}/>
             <label >Rating</label>
             <input type="number" name='rating' value={input.rating} onChange={(e) => handleChange(e)}/>
+            {
+                errors.rating && (
+                    <p>{errors.rating}</p>
+                )
+            }
             <label>Image</label>
             <input type="text" name='image' value={input.image} onChange={(e) => handleChange(e)}/>
             <button>CREATED GAME</button>
