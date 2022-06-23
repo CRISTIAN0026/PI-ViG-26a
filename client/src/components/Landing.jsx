a361fc93c5c29de32ba3647fdc8c8287ce3d8e47
CRISTIAN0026 <92878127+CRISTIAN0026@users.noreply.github.com>
2022-06-23 11:54:32 -0500

    10 commit

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import './Landing.css';
 
 const Landing = () => {
 
     return( 
-        <div>
+        <div id="landing">
+            <div>
             <h1>Welcome to the world of video games</h1>
+            </div>
+            <div>
             <Link to='/home'><button>SEE VIDEO GAMES</button></Link>
+            </div>
         </div>
     )
 }
