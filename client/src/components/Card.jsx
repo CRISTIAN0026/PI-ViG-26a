f68439faf475adb0c22e074ca73822c170891c1e
CRISTIAN0026 <92878127+CRISTIAN0026@users.noreply.github.com>
2022-06-30 17:45:57 -0500

    tas

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Card.css'
 
-const Card = ({name, image, genres, released }) =>{
+const Card = ({name, image, genres, rating }) =>{
     
     return(
         <div className='carM'>
             <img src={image} alt="not found" width='200px' height='200px' className='imt'/>
             <div className='yt'>
-                <h1>{released}</h1>
+                <h3>{rating}</h3>
             <h1  className='st'>{name}</h1>
             </div>
             <h5 className='gth'>{genres + ' '}</h5>
